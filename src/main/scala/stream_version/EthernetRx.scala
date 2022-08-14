471a005ba780b06923cfef410106dd4f58662e4f
dzx-dzx <dzx_dzx@hotmail.com>
2022-08-14 11:12:29 +0800

    Refactor Header.

--- src/main/scala/stream_version/EthernetRx.scala
+++ src/main/scala/stream_version/EthernetRx.scala
@@ -2,6 +2,8 @@ package udp_master_stream
 
 import spinal.core._
 import spinal.lib._
+import EthernetUserConstant._
+import EthernetProtocolConstant._
 
 case class EthernetRxGenerics(
     destIp: Long,  // Destination IP address 32bit
@@ -34,47 +36,20 @@ object StreamExtractCompany {
     }.outputStream
 }
 
-case class EthernetHeadRx() extends Bundle {
-  val udpCheckSum = Bits(EthernetProtocolConstant.UDP_CHECKSUM_WIDTH bits)
-  val udpLength   = Bits(EthernetProtocolConstant.UDP_LENGTH_WIDTH bits)
-  val destPort    = Bits(EthernetProtocolConstant.PORT_WIDTH bits)
-  val srcPort     = Bits(EthernetProtocolConstant.PORT_WIDTH bits)
-  val destIp      = Bits(EthernetProtocolConstant.IP_WIDTH bits)
-  val srcIp       = Bits(EthernetProtocolConstant.IP_WIDTH bits)
-  val headerCheckSum = Bits(
-    EthernetProtocolConstant.HEADER_CHECKSUM_WIDTH bits
-  )
-  val ttlProtocol = Bits(EthernetProtocolConstant.TTL_PROTOCOL_WIDTH bits)
-  val flagsFragmentOffset = Bits(
-    EthernetProtocolConstant.FLAGS_FRAGMENT_OFFSET_WIDTH bits
-  )
-  val identification = Bits(
-    EthernetProtocolConstant.IDENTIFICATION_WIDTH bits
-  )
-  val totalLength = Bits(EthernetProtocolConstant.TOTAL_LENGTH_WIDTH bits)
-  val tos         = Bits(EthernetProtocolConstant.TOS_WIDTH bits)
-  val versionIhl  = Bits(EthernetProtocolConstant.VERSION_IHL_WIDTH bits)
-  val ethType     = Bits(EthernetProtocolConstant.ETH_TYPE_WIDTH bits)
-  val srcMac      = Bits(EthernetProtocolConstant.MAC_WIDTH bits)
-  val destMac     = Bits(EthernetProtocolConstant.MAC_WIDTH bits)
-  val preambleSdf = Bits(EthernetProtocolConstant.PREAMBLE_SDF_WIDTH bits)
-
-}
-
 case class EthernetRxDataEth() extends Bundle {
-  val eth   = EthernetHeadRx()
+  val eth   = Header()
   val input = Fragment(EthernetRxDataIn())
 }
 
 case class EthernetRxDataIn() extends Bundle {
-  val data  = Bits(EthernetUserConstant.DATA_WIDTH bits)
-  val tkeep = Bits(EthernetUserConstant.KEEP_WIDTH bits)
+  val data  = Bits(DATA_WIDTH bits)
+  val tkeep = Bits(KEEP_WIDTH bits)
 }
 
 case class EthernetRxDataOut() extends Bundle {
-  val data = Bits(EthernetUserConstant.DATA_WIDTH bits)
-//  val byteNum = UInt(EthernetUserConstant.BYTE_NUM_WIDTH bits)
-  val tkeep = Bits(EthernetUserConstant.KEEP_WIDTH bits)
+  val data = Bits(DATA_WIDTH bits)
+//  val byteNum = UInt(BYTE_NUM_WIDTH bits)
+  val tkeep = Bits(KEEP_WIDTH bits)
 }
 
 case class EthernetRx(ethernetRxGenerics: EthernetRxGenerics) extends Component {
@@ -95,7 +70,7 @@ case class EthernetRx(ethernetRxGenerics: EthernetRxGenerics) extends Component 
         result.valid := False
       }
     }.result
-    //提取包头数据并维持在处理整个包时有效, 直到下一个包头.
+  // 提取包头数据并维持在处理整个包时有效, 直到下一个包头.
 
   val dataInExtractCompany = StreamExtractCompany(io.dataIn, companyExtractFunc)
 
@@ -104,7 +79,7 @@ case class EthernetRx(ethernetRxGenerics: EthernetRxGenerics) extends Component 
   )
 
   val inputData = Stream(Fragment(EthernetRxDataIn()))
-  val inputEth  = Stream(EthernetHeadRx())
+  val inputEth  = Stream(Header())
 
   inputData << dataInExtractCompany1.translateWith {
     val ret = Fragment(EthernetRxDataIn())
@@ -115,45 +90,11 @@ case class EthernetRx(ethernetRxGenerics: EthernetRxGenerics) extends Component 
   }
 
   inputEth << dataInExtractCompany2.translateWith {
-    val ret = EthernetHeadRx()
-    ret.assignFromBits(
-      dataInExtractCompany._2.data(
-        EthernetUserConstant.DATA_WIDTH - 1 downto EthernetUserConstant.DATA_WIDTH -
-          EthernetProtocolConstant.BYTE_WIDTH * EthernetProtocolConstant.ETH_TOTAL_LENGTH
-      )
+    val ret = Header()
+    ret.set(
+      dataInExtractCompany._2.data(DATA_WIDTH - 1 downto DATA_WIDTH - BYTE_WIDTH * ETH_TOTAL_LENGTH)
     )
     ret
-  }
-
-  def EthMacCheck(
-      tkeep: Bits,
-      eth: EthernetHeadRx
-  ): Bool = {
-    val fit = {
-      ~(tkeep(
-        EthernetUserConstant.KEEP_WIDTH - 1 downto EthernetUserConstant.KEEP_WIDTH - EthernetProtocolConstant.ETH_TOTAL_LENGTH
-      ) === B(
-        EthernetProtocolConstant.ETH_TOTAL_LENGTH bits,
-        default -> True
-      ) & (eth.preambleSdf === EthernetProtocolConstant.PREAMBLE_SDF) && ((eth.destMac === ethernetRxGenerics.destMac) || (eth.destMac === EthernetProtocolConstant.BROADCAST_MAC_ADDRESS)) && (eth.ethType === EthernetProtocolConstant.ETH_TYPE))
-    }
-    fit
-  }
-
-  def EthIpCheck(
-      eth: EthernetHeadRx
-  ): Bool = {
-    val fit = ~((eth.ttlProtocol(
-      (EthernetProtocolConstant.TTL_PROTOCOL_WIDTH / 2 - 1) downto 0
-    ) === EthernetProtocolConstant.PROTOCOL) && (eth.destIp === ethernetRxGenerics.destIp))
-    fit
-  }
-
-  def EthUdpCheck(
-      eth: EthernetHeadRx
-  ): Bool = {
-    val fit = ~(eth.destPort === ethernetRxGenerics.destPort)
-    fit
   }
 
   val eth = StreamJoin
@@ -163,33 +104,46 @@ case class EthernetRx(ethernetRxGenerics: EthernetRxGenerics) extends Component 
       ret.input.tkeep := inputData.tkeep
       ret.input.data  := inputData.data
       ret.input.last  := inputData.last
-      ret.eth         := inputEth.payload//包传递完成前保持不变.
+      ret.eth         := inputEth.payload // 包传递完成前保持不变.
       ret
     }
 
-  val ethMacCheck = Stream(EthernetRxDataEth())
-  ethMacCheck <-/< eth.throwWhen(
-    EthMacCheck(ethMacCheck.input.tkeep, ethMacCheck.eth)
+  def EthMacCheck(tkeep: Bits, mac: MacHeader): Bool = ~(
+    tkeep(KEEP_WIDTH - 1 downto KEEP_WIDTH - ETH_TOTAL_LENGTH) === B(ETH_TOTAL_LENGTH bits, default -> True)
+      && (mac.preambleSdf === PREAMBLE_SDF)
+      && ((mac.destMac === ethernetRxGenerics.destMac) || (mac.destMac === BROADCAST_MAC_ADDRESS))
+      && (mac.ethType === ETH_TYPE)
   )
+
+  def EthIpCheck(ip: IPHeader): Bool = ~(
+    ip.ttlProtocol((TTL_PROTOCOL_WIDTH / 2 - 1) downto 0) === PROTOCOL
+      && (ip.destIp === ethernetRxGenerics.destIp)
+  )
+
+  def EthUdpCheck(udp: UDPHeader): Bool = ~(udp.destPort === ethernetRxGenerics.destPort)
+  // 校验包头各部分.
+
+  val ethMacCheck = Stream(EthernetRxDataEth())
+  ethMacCheck <-/< eth.throwWhen {
+    EthMacCheck(ethMacCheck.input.tkeep, ethMacCheck.eth.mac)
+  }
 
   val ethIpCheck = Stream(EthernetRxDataEth())
   ethIpCheck <-/< ethMacCheck.throwWhen(
-    EthIpCheck(ethIpCheck.eth)
+    EthIpCheck(ethIpCheck.eth.ip)
   )
 
   val ethUdpCheck = Stream(EthernetRxDataEth())
   ethUdpCheck <-/< ethIpCheck.throwWhen(
-    EthUdpCheck(ethUdpCheck.eth)
+    EthUdpCheck(ethUdpCheck.eth.udp)
   )
-  //校验包头各部分.
 
   io.dataOut <-/< ethUdpCheck.translateWith {
-    val ret = Fragment(EthernetRxDataOut())
-    ret.tkeep := ethUdpCheck.input.tkeep
-    ret.data  := ethUdpCheck.input.data
-//    ret.byteNum := ethUdpCheck.eth.udpLength.asUInt - EthernetProtocolConstant.UDP_ETH_LENGTH
-    ret.last := ethUdpCheck.input.last
-    ret
-  }
-
+      val ret = Fragment(EthernetRxDataOut())
+      ret.tkeep := ethUdpCheck.input.tkeep
+      ret.data  := ethUdpCheck.input.data
+//    ret.byteNum := ethUdpCheck.eth.udpLength.asUInt - UDP_ETH_LENGTH
+      ret.last := ethUdpCheck.input.last
+      ret
+    }
 }
