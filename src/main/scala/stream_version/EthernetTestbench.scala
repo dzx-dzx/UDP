ac8a4b8bdf3b363db5e0b3091a7d98e25c18721d
dzx-dzx <dzx_dzx@hotmail.com>
2022-08-20 12:44:26 +0800

    Remove redundant modules. (There's no connection between them)

--- src/main/scala/stream_version/EthernetTestbench.scala
+++ src/main/scala/stream_version/EthernetTestbench.scala
@@ -11,15 +11,9 @@ class udp_40G_TOP() extends BlackBox {
     val gt_rxn_in_40MAC_0    = in Bits (4 bits)
     val gt_txp_out_40MAC_0   = out Bits (4 bits)
     val gt_txn_out_40MAC_0   = out Bits (4 bits)
-    val gt_rxp_in_40MAC_1    = in Bits (4 bits)
-    val gt_rxn_in_40MAC_1    = in Bits (4 bits)
-    val gt_txp_out_40MAC_1   = out Bits (4 bits)
-    val gt_txn_out_40MAC_1   = out Bits (4 bits)
     val sys_reset            = in Bool ()
     val gt_ref_clk_p_40MAC_0 = in Bool ()
     val gt_ref_clk_n_40MAC_0 = in Bool ()
-    val gt_ref_clk_p_40MAC_1 = in Bool ()
-    val gt_ref_clk_n_40MAC_1 = in Bool ()
     val sys_clk_p            = in Bool ()
     val sys_clk_n            = in Bool ()
   }
@@ -27,47 +21,37 @@ class udp_40G_TOP() extends BlackBox {
   mapCurrentClockDomain(clock = io.clk, reset = io.sys_reset)
 
   addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/udp_40G_Top.v")
-  addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/l_ethernet_0_exdes_1.v")
   addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/l_ethernet_0_exdes.v")
   addRTLPath(
     "/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/l_ethernet_0_mac_baser_syncer_reset.v"
   )
   addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/TxFsm.v")
 
-  // addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/Sources/EthernetTx.v")
+  addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/Sources/EthernetTx.v")
   // addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/Sources/EthernetRx.v")
 
-  addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/EthernetTx.v")
+  // addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/EthernetTx.v")
   addRTLPath("/home/dzx/Programming/High-speed-UDP-transport-protocol/VIVADO/Sources/EthernetRx.v")
 }
 
 class TopLevel extends Component {
   val io = new Bundle {
     val gt_ref_clk_p_40MAC_0 = in Bool ()
-    val gt_ref_clk_p_40MAC_1 = in Bool ()
     val sys_clk_p            = in Bool ()
     val gt_txp_out_40MAC_0   = out Bits (4 bits)
     val gt_txn_out_40MAC_0   = out Bits (4 bits)
-    val gt_txp_out_40MAC_1   = out Bits (4 bits)
-    val gt_txn_out_40MAC_1   = out Bits (4 bits)
   }
   val udp = new udp_40G_TOP()
   udp.io.gt_ref_clk_p_40MAC_0 := io.gt_ref_clk_p_40MAC_0
-  udp.io.gt_ref_clk_p_40MAC_1 := io.gt_ref_clk_p_40MAC_1
   udp.io.sys_clk_p            := io.sys_clk_p
   udp.io.gt_ref_clk_n_40MAC_0 := !io.gt_ref_clk_p_40MAC_0
-  udp.io.gt_ref_clk_n_40MAC_1 := !io.gt_ref_clk_p_40MAC_1
   udp.io.sys_clk_n            := !io.sys_clk_p
 
   udp.io.gt_txp_out_40MAC_0 <> io.gt_txp_out_40MAC_0
   udp.io.gt_txn_out_40MAC_0 <> io.gt_txn_out_40MAC_0
-  udp.io.gt_txp_out_40MAC_1 <> io.gt_txp_out_40MAC_1
-  udp.io.gt_txn_out_40MAC_1 <> io.gt_txn_out_40MAC_1
 
   udp.io.gt_rxp_in_40MAC_0 <> udp.io.gt_txp_out_40MAC_0
   udp.io.gt_rxn_in_40MAC_0 <> udp.io.gt_txn_out_40MAC_0
-  udp.io.gt_rxp_in_40MAC_1 <> udp.io.gt_txp_out_40MAC_1
-  udp.io.gt_rxn_in_40MAC_1 <> udp.io.gt_txn_out_40MAC_1
 
 }
 
@@ -86,13 +70,11 @@ object EthernetTestbench {
       .doSim(dut => {
         dut.clockDomain.forkStimulus(period = 10)
         dut.io.gt_ref_clk_p_40MAC_0 #= 1.toBoolean
-        dut.io.gt_ref_clk_p_40MAC_1 #= 1.toBoolean
         dut.io.sys_clk_p #= 1.toBoolean
-        for (_ <- 0 until 100000) {
+        for (_ <- 0 until 100) {
           fork {
             dut.clockDomain.waitSampling(48)
             dut.io.gt_ref_clk_p_40MAC_0 #= !dut.io.gt_ref_clk_p_40MAC_0.toBoolean
-            dut.io.gt_ref_clk_p_40MAC_1 #= !dut.io.gt_ref_clk_p_40MAC_1.toBoolean
           }
           fork {
             dut.clockDomain.waitSampling(25)
